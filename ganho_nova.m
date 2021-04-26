ad17c2f0cddcca054a5e474a268d266938b26d94
pedro7744 <75043682+pedro7744@users.noreply.github.com>
2021-04-26 20:51:26 -0300

    Update ganho_nova.m

--- ganho_nova.m
+++ ganho_nova.m
@@ -9,7 +9,7 @@ warning off all
 % D = pop(4);
 % GdB = A/(sqrt(B + C*Pin^D));
 
-Pinf = 10*log10(Pinf*1e3);  %Tá em dBm
+%Pinf = 10*log10(Pinf);  %Tá em dBm
 syms G Psat Pin Gmax; 
 s = 'G=1+(Psat/Pin)*log(Gmax/G)';
 s = subs(s,[Psat, Pin, Gmax],[GmaxPsat(2), Pinf, GmaxPsat(1)]);
