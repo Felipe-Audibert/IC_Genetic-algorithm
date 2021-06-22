306b8d7a4f2e69d06f28860a0d34df2ea1352529
pedro7744 <75043682+pedro7744@users.noreply.github.com>
2021-06-21 21:24:08 -0300

    120, 150, 280

--- save_figures.m
+++ save_figures.m
@@ -7,5 +7,6 @@ Vec_Pin = [0.43 0.68 1 4.01 14]*1e-3;
 for Pin = Vec_Pin
     [line col] = find(Storage_Diff(:,:,find(Vec_Pin==Pin))==min(min(Storage_Diff(:,:,find(Vec_Pin==Pin)))));
     cascateado_dudu(EDFA, Pin, Storage_Pop(line(1),1,col(1),find(Vec_Pin==Pin)), Storage_Pop(line(1),2,col(1),find(Vec_Pin==Pin)),0,1);
+    close all
 end
 end
