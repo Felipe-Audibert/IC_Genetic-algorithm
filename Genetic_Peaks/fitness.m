b0377b2fdc69daa3514695d810f8b6e6eefe7deb
pedro7744 <pedrosoares77744@gmail.com>
2022-03-09 17:50:24 -0300

    Genetic_peaks update

--- Genetic_Peaks/fitness.m
+++ Genetic_Peaks/fitness.m
@@ -1,9 +1,8 @@
 function [Vec_Prop, Simu] = fitness(EDFA, Pin, Pop, Popsize, Ctrl)
     
-    Simu = zeros(1,Popsize);
+    Peaks = zeros(1,Popsize);
     for i=1:Popsize
-        Simu(i)          = cascateado(EDFA, Pin, Pop(i, :), 0, 0);
-        Peaks(i)         = sum(Simu(i)>1e-3);
+        Peaks(i)          = sum(cascateado(EDFA, Pin, Pop(i, :), 0, 0)>1e-3);
     end
     maximo = max(Peaks);
     Vec_Prop                                = (Peaks./maximo).^Ctrl;
